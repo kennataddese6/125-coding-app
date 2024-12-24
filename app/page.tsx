5edc4c842afbbc1097661fa230c5dd7971ef7801
kennataddese6 <kennataddese6@gmail.com>
2024-12-24 16:21:39 +0300

    separte hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,21 @@
 import BrandWorks from 'components/brand-works';
 import Hero from 'components/hero';
-/* export const metadata = {
+import HomeProducts from 'components/home-products';
+
+export const metadata = {
   description: 'High-performance ecommerce store built with Next.js, Vercel, and Wix.',
   openGraph: {
     type: 'website'
   }
 };
- */
+
 export default function HomePage() {
   return (
     <>
       <Hero />
       <br />
       <BrandWorks />
-
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
-      <h1>Hello world</h1>
+      <HomeProducts />
       {/* <ThreeItemGrid /  >
       <Carousel />
       <Footer /> */}
