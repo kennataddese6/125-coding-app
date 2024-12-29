b160bbb71199ad85da2442307fe131cac776d12d
kennataddese6 <kennataddese6@gmail.com>
2024-12-29 22:11:28 +0300

    add filter icon on mobile

--- components/sort-dropdown.tsx
+++ components/sort-dropdown.tsx
@@ -1,23 +1,28 @@
 'use client';
 
 import useChangeUrl from 'lib/change-url';
-
+import { CiFilter } from 'react-icons/ci';
 export default function SortDropDown() {
   const { createSortQuery, deleteSortQuery } = useChangeUrl();
   return (
-    <div>
-      <label className="mr-3 font-bold">Sort by</label>
-      <select
-        onChange={(e) => {
-          e.target.value ? createSortQuery(e.target.value) : deleteSortQuery(e.target.value);
-        }}
-      >
-        <option value={''}>Relevance</option>
-        <option value={'trending-desc'}>Trending</option>
-        <option value={'latest-desc'}>Latest arrivals</option>
-        <option value={'price-asc'}>Price: Low to high</option>
-        <option value={'price-desc'}>Price: High to low</option>
-      </select>
-    </div>
+    <>
+      <div className="rounded-3xl border border-stone-400 px-4 py-2 font-bold lg:hidden">
+        Filter and sort <CiFilter className="inline" color="black" />
+      </div>
+      <div className="hidden lg:block">
+        <label className="mr-3 font-bold">Sort by</label>
+        <select
+          onChange={(e) => {
+            e.target.value ? createSortQuery(e.target.value) : deleteSortQuery(e.target.value);
+          }}
+        >
+          <option value={''}>Relevance</option>
+          <option value={'trending-desc'}>Trending</option>
+          <option value={'latest-desc'}>Latest arrivals</option>
+          <option value={'price-asc'}>Price: Low to high</option>
+          <option value={'price-desc'}>Price: High to low</option>
+        </select>
+      </div>
+    </>
   );
 }
