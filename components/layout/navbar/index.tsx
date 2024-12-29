879f16a419372f25af59fccaf7fbf3a52df9c322
kennataddese6 <kennataddese6@gmail.com>
2024-12-29 12:34:25 +0300

    add navbar background color

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -1,20 +1,35 @@
+'use client';
 import CartModal from 'components/cart/modal';
-import { getMenu } from 'lib/shopify';
 import { Menu } from 'lib/shopify/types';
 import Link from 'next/link';
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import MobileMenu from './mobile-menu';
 
 const { SITE_NAME } = process.env;
 
-export async function Navbar() {
-  const menu = await getMenu('next-js-frontend-header-menu');
+export function Navbar() {
+  // const menu = await getMenu('next-js-frontend-header-menu');
+  const [showNav, setShowNav] = useState(false);
 
+  const menu = [];
+  function scrollPosition() {
+    const position = document.documentElement.scrollTop;
+    if (position > 225) {
+      setShowNav(true);
+    } else {
+      setShowNav(false);
+    }
+  }
+  window.addEventListener('scroll', () => {
+    scrollPosition();
+  });
   return (
-    <nav className="fixed flex w-full items-center justify-between p-4 lg:px-6">
+    <nav
+      className={`fixed z-20 flex w-full items-center justify-between p-4 lg:px-6 ${showNav ? 'animate-fadeIn bg-white/60 text-black backdrop-blur-xl' : ''}`}
+    >
       <div className="block flex-none md:hidden">
         <Suspense fallback={null}>
-          <MobileMenu menu={menu} />
+          <MobileMenu menu={[]} />
         </Suspense>
       </div>
       <div className="flex w-full items-center justify-between">
@@ -26,12 +41,12 @@ export async function Navbar() {
           >
             {/* <LogoSquare /> */}
             <div className="ml-2 flex-none text-sm font-medium uppercase md:hidden lg:block">
-              {SITE_NAME}
+              125CODING
             </div>
           </Link>
           {menu.length ? (
             <ul className="hidden gap-6 text-sm md:flex md:items-center">
-              {menu.map((item: Menu) => (
+              {[].map((item: Menu) => (
                 <li key={item.title}>
                   <Link
                     href={item.path}
@@ -52,17 +67,21 @@ export async function Navbar() {
         </div> */}
         <div className="hidden items-center justify-end md:flex">
           <ul className="flex">
-            <li className="mx-5 cursor-pointer rounded-md border-b-2 border-b-white p-1">Home</li>
+            <li
+              className={`mx-5 cursor-pointer rounded-md border-b-2 p-1 ${showNav ? 'border-b-transparent text-blue-600' : 'border-b-white'}`}
+            >
+              Home
+            </li>
             <li className="mx-5 cursor-pointer p-1">
               <Link href={'/products'}>Products</Link>
             </li>
             <li className="mx-5 cursor-pointer p-1">About</li>
             <li className="mx-5 cursor-pointer p-1">Contact</li>
           </ul>
-          <CartModal color="text-white" />
+          <CartModal color={`${showNav ? 'text-black' : 'text-white'}`} />
         </div>
         <div className="lg:hidden">
-          <CartModal color="text-white" />
+          <CartModal color={`${showNav ? 'text-black' : 'text-white'}`} />
         </div>
       </div>
     </nav>
