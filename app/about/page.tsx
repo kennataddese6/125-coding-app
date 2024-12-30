17a6da3a5023b007421ed5787fcc0c054f0a405c
kennataddese6 <kennataddese6@gmail.com>
2024-12-30 23:14:19 +0300

    add faq

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,10 @@
+'use client';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger
+} from '@/components/ui/accordion';
 import NewFooter from 'components/footer';
 import { BlackNavbar } from 'components/layout/navbar/black-navbar';
 
@@ -17,6 +24,63 @@ export default function Page() {
           <button className="mx-auto mt-3 block rounded-3xl bg-blue-500 px-6 py-2 text-white lg:mx-0">
             Read More
           </button>
+          <h1 className="mt-8 text-center text-xl lg:text-start">Frequently Asked Questions</h1>
+          <Accordion type="multiple" className="mt-4 w-full">
+            <AccordionItem value="item-1">
+              <AccordionTrigger>What is Adaptations and Coding?</AccordionTrigger>
+              <AccordionContent>
+                Car Coding is a relatively new thing in the Automotive World, and it implies
+                software modifications to a Vehicle to allow the use of functions not available in
+                the Car when it left the factory for different reasons, mainly local regulations
+                (Some countries have different requirements from when the car leaving the factory).
+                Coding does not imply changing the software! It only changes pre-programmed options
+                already existing in the software. In modern Vehicles, there are functions that the
+                manufacturer has not activated. Changing these settings is called Coding.
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-2">
+              <AccordionTrigger>What can Car Coding bring to my Vehicle?</AccordionTrigger>
+              <AccordionContent>
+                As we explained above, most cars have country-specific settings for their features,
+                most vehicles have limitations and other restrictions. With the help of coding, the
+                owner can change the chimes on their car for when they lock and unlock, set how many
+                times its one-touch signal flickers for the indicator, remember settings for
+                Start/Stop systems, and more. Coders can also activate cornering headlights with fog
+                lights, deactivate some buzzing sounds, and enable multiple functions of the comfort
+                module. Other possibilities involve lowering the level of the passenger-side mirror
+                when reversing, activate the “needle sweep” function when the ignition is on (only
+                for some Volkswagen AG models), automatic door locking at a particular speed,
+                U.S.-style daytime running lights, the auto-hold function for vehicles with
+                electrically operated parking brake, and others.
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-3">
+              <AccordionTrigger>Do we travel?</AccordionTrigger>
+              <AccordionContent>
+                Yes. Let us know what you need doing and we will price up a quote to come out to you
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-4">
+              <AccordionTrigger>
+                What if I have Warranty? Will coding void my Warranty?
+              </AccordionTrigger>
+              <AccordionContent>
+                This is a question always asked which is a Yes & No. Yes depending on dealership as
+                they always try to wriggle out of any warranty claim. No as we offer a free service
+                to take off any coding before your vehicle goes in for service/warranty work and
+                reapply free of charge after. ​
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-5">
+              <AccordionTrigger>How long does coding take?</AccordionTrigger>
+              <AccordionContent>
+                Generally around 45 minutes depending on what you require! We never like to rush and
+                we will go at the customer's pace. We will always allow the customer to test and
+                play around with any feature we have activated. We will explain the process and
+                ensure you are fully confident in the work we have carried out.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
         </div>
         {/*     <div>
           <Image src={'/team.png'} width={500} height={600} alt="team image" />
