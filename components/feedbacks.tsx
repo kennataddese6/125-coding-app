282ae24747baf271f6a8c4892072777706e93b69
kennataddese6 <kennataddese6@gmail.com>
2025-01-07 20:22:40 +0300

    add motion on feedback

--- components/feedbacks.tsx
+++ components/feedbacks.tsx
@@ -1,13 +1,26 @@
+'use client';
+import { motion } from 'motion/react';
 import { FaStar } from 'react-icons/fa';
 import { MdVerified } from 'react-icons/md';
-
 export default function FeedBack() {
   return (
     <div className="bg-white p-4 text-black lg:mx-8">
       <div className="mx-auto mt-4 p-4 text-black lg:w-4/5">
-        <h2 className="text-center text-2xl font-bold lg:text-start">What our customers say</h2>
+        <motion.h2
+          className="text-center text-2xl font-bold lg:text-start"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.5, ease: 'easeOut' }}
+        >
+          What our customers say
+        </motion.h2>
         <div className="my-10 flex flex-wrap justify-between gap-4">
-          <div className="max-w-72">
+          <motion.div
+            className="max-w-72"
+            initial={{ opacity: 0, translateY: 50 }}
+            whileInView={{ opacity: 1, translateY: 0 }}
+            transition={{ duration: 0.5, delay: 0.5, ease: 'easeOut' }}
+          >
             <FaStar className="inline" color="gold" />
             <FaStar className="ml-0.5 inline" color="gold" />
             <FaStar className="ml-0.5 inline" color="gold" />
@@ -20,9 +33,14 @@ export default function FeedBack() {
               Buzzing to get rid of buzzing on my sprinter, fantastic service could highly recomm...
             </p>
             <h3 className="my-1 font-semibold">Julian Cooke</h3>
-          </div>
+          </motion.div>
 
-          <div className="max-w-72">
+          <motion.div
+            className="max-w-72"
+            initial={{ opacity: 0, translateY: 50 }}
+            whileInView={{ opacity: 1, translateY: 0 }}
+            transition={{ duration: 0.5, delay: 0.7, ease: 'easeOut' }}
+          >
             <FaStar className="inline" color="gold" />
             <FaStar className="ml-0.5 inline" color="gold" />
             <FaStar className="ml-0.5 inline" color="gold" />
@@ -36,8 +54,13 @@ export default function FeedBack() {
               nice...
             </p>
             <h3 className="my-1 font-semibold">Will Naylor</h3>
-          </div>
-          <div className="max-w-72">
+          </motion.div>
+          <motion.div
+            className="max-w-72"
+            initial={{ opacity: 0, translateY: 50 }}
+            whileInView={{ opacity: 1, translateY: 0 }}
+            transition={{ duration: 0.5, delay: 0.9, ease: 'easeOut' }}
+          >
             <FaStar className="inline" color="gold" />
             <FaStar className="ml-0.5 inline" color="gold" />
             <FaStar className="ml-0.5 inline" color="gold" />
@@ -51,7 +74,7 @@ export default function FeedBack() {
               in my vw Golf which was fitted{' '}
             </p>
             <h3 className="my-1 font-semibold">Adam Jasat</h3>
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
